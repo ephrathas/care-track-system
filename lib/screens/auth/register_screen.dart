c2ac559c6b6547645611363ac2dda42b91a30ee5
Mubarek Jemal <mubarekj71@gmail.com>
2026-05-17 00:30:15 +0300

    Role dropdown for Register Screen

--- lib/screens/auth/register_screen.dart
+++ lib/screens/auth/register_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
 
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  String _selectedRole = 'Parent';
+  final List<String> _roles = ['Parent', 'Teacher', 'Child', 'Healthcare'];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,14 +18,35 @@ class RegisterScreen extends StatelessWidget {
         padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
-            const TextField(
-                decoration: InputDecoration(labelText: "Full Name")),
+            const TextField(decoration: InputDecoration(labelText: "Full Name")),
             const TextField(decoration: InputDecoration(labelText: "Email")),
             const TextField(
-                obscureText: true,
-                decoration:
-                    InputDecoration(labelText: "Password", )),
+              obscureText: true,
+              decoration: InputDecoration(labelText: "Password", )),
             const SizedBox(height: 30),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(color: Colors.grey.shade400),
+              ),
+              child: DropdownButtonHideUnderline(
+                child: DropdownButton<String>(
+                  value: _selectedRole,
+                  isExpanded: true,
+                  items: _roles.map((String role) {
+                    return DropdownMenuItem(
+                      value: role,
+                      child: Text(role),
+                    );
+                  }).toList(),
+                  onChanged: (newValue) {
+                    // You will need to wrap this in setState() if using a StatefulWidget
+                    print("Selected role: $newValue");
+                  },
+                ),
+              ),
+            ),
             ElevatedButton(
               onPressed: () {},
               child: const Text("Sign Up"),
