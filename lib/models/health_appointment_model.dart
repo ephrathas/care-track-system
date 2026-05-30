77a8602e5260ef8d1c18499e3096159a80478e26
ephratha samuel <sephratha@gmail.com>
2026-05-30 19:00:26 +0300

    helth appointment

--- lib/models/health_appointment_model.dart
+++ lib/models/health_appointment_model.dart
@@ -2,5 +2,18 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
 
 class HealthAppointment {
-  
+  final String id;
+  final String childId;
+  final String childName;
+  final String title;
+  final DateTime scheduledAt;
+  final String status;
+ HealthAppointment({
+    required this.id,
+    required this.childId,
+    required this.childName,
+    required this.title,
+    required this.scheduledAt,
+    this.status = 'Active',
+  }); 
 }
